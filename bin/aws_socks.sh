ae90049f8f260aaa50059092d724aebd3c8304d2
david-stratusee <david@stratusee.com>
2015-01-11 10:31:59 +0800

    fix

--- bin/aws_socks.sh
+++ bin/aws_socks.sh
@@ -17,9 +17,8 @@ set -o nounset                              # Treat unset variables as an error
 #-------------------------------------------------------------------------------
 username=david
 
-#host_port="dev-aie.stratusee.com:22"
-host_port="54.174.130.103:22"
-#host_port="us.stratusee.com:2221"
+available_host_port=("dev-aie.stratusee.com:22" "54.174.130.103:22" "us.stratusee.com:2221")
+host_port=${available_host_port[1]}
 
 ETH="Wi-Fi"
 #-------------------------------------------------------------------------------
@@ -51,8 +50,8 @@ function fill_and_run_proxy()
     echo "while [ 1 -eq 1 ]; do" >>/tmp/watch_socks.sh
     echo "    pidcount=\`ps -ef | grep -v grep | grep -c \"ssh -D\"\`" >>/tmp/watch_socks.sh
     echo "    if [ \$pidcount -eq 0 ]; then" >>/tmp/watch_socks.sh
-    echo "        echo -e \" [\"\`date +'%H:%M:%S'\`\"] ssh -D 8099 -f -q -C -N ${username}@${remote_host}${remote_port}\"" >>/tmp/watch_socks.sh
-    echo "        ssh -D 8099 -f -q -C -N ${username}@${remote_host}${remote_port}" >>/tmp/watch_socks.sh
+    echo "        echo -e \" [\"\`date +'%H:%M:%S'\`\"] ssh -D 8099 -fqCnN ${username}@${remote_host}${remote_port}\"" >>/tmp/watch_socks.sh
+    echo "        ssh -D 8099 -fqCnN ${username}@${remote_host}${remote_port}" >>/tmp/watch_socks.sh
     echo "    fi" >>/tmp/watch_socks.sh
     echo "    sleep 1" >>/tmp/watch_socks.sh
     echo "done" >>/tmp/watch_socks.sh
@@ -71,13 +70,67 @@ function kill_process()
     fi
 }
 
-if [ $# -gt 0 ] && [ "$1" == "-h" ]; then
-    echo "-c for clear socks proxy"
-    echo "-l for  show socks proxy"
-    echo "ip for   set socks proxy and http proxy"
-    echo "no args for set socks proxy and DIRECT"
-    exit 0
-elif [ $# -gt 0 ] && [ "$1" == "-c" ]; then
+function print_avail_host()
+{
+    echo "available socks host:port is:"
+    for node in ${available_host_port[*]}; do
+        echo "* $node"
+    done
+}
+
+IP=""
+MODE="normal"
+while getopts 'e:p:i:hcl' opt; do
+    case $opt in
+        e) 
+            ETH=$OPTARG
+            ;;
+        i)  
+            IP=$OPTARG
+            ping -q -c 1 ${IP} >/dev/null
+            if [ $? -ne 0 ]; then
+                echo "${IP} is unreachable"
+                exit 1
+            fi
+            ;;
+        c)
+            if [ "${MODE}" == "normal" ]; then
+                MODE="clear"
+            else
+                echo "clear and query mode should not be used at same time"
+            fi
+            ;;
+        l)
+            if [ "${MODE}" == "normal" ]; then
+                MODE="query"
+            else
+                echo "clear and query mode should not be used at same time"
+            fi
+            ;;
+        p)
+            host_port=$OPTARG
+            comma_count=`echo $host_port | grep -c ":"`
+            if [ ${comma_count} -eq 0 ]; then
+                echo "invalid host_port format"
+                $0 -h
+                exit 1
+            fi
+            ;;
+        h|*)
+            echo "------------------------------------"
+            echo "Help Usage: "
+            echo "-c for clear socks proxy"
+            echo "-l for query socks proxy"
+            echo "-i ip for set socks proxy and http proxy"
+            echo "-p to set socks proxy's host_port, format: proxy:port"
+            print_avail_host
+            echo "no args for set socks proxy and DIRECT"
+            echo "------------------------------------"
+            exit 0
+    esac
+done
+
+if [ "${MODE}" == "clear" ]; then
     kill_process "watch_socks"
     kill_process "ssh -D"
 
@@ -86,18 +139,15 @@ elif [ $# -gt 0 ] && [ "$1" == "-c" ]; then
         sudo apachectl graceful-stop
     fi
     sudo networksetup -setautoproxystate ${ETH} off
-elif [ $# -gt 0 ] && [ "$1" == "-l" ]; then
-    show_proxy
-    exit 0
-else
+elif [ "${MODE}" == "normal" ]; then
     sudo networksetup -setautoproxystate ${ETH} off
-    if [ $# -gt 0 ]; then
+    if [ "${IP}" == "" ]; then
+        sudo networksetup -setautoproxyurl ${ETH} "https://david-stratusee.github.io/proxy.pac"
+    else
         sudo cp -f /Library/WebServer/Documents/proxy.pac /Library/WebServer/Documents/proxy_aie.pac
-        sudo sed -i -e "s/'DIRECT'/'PROXY $1:3128'/g" /Library/WebServer/Documents/proxy_aie.pac
+        sudo sed -i "" -e "s/'DIRECT'/'PROXY ${IP}:3128'/g" /Library/WebServer/Documents/proxy_aie.pac
         sudo networksetup -setautoproxyurl ${ETH} "http://127.0.0.1/proxy_aie.pac"
         sudo apachectl start
-    else
-        sudo networksetup -setautoproxyurl ${ETH} "https://david-stratusee.github.io/proxy.pac"
     fi
 
     echo start socks
@@ -105,6 +155,5 @@ else
 
     sudo networksetup -setautoproxystate ${ETH} on
 fi
-
 show_proxy
 
