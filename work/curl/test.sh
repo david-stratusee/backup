f75861c85193939ab5e65a559622b52ffa8fbfd1
david-stratusee <david@stratusee.com>
2015-01-23 15:50:03 +0800

    fix

--- work/curl/test.sh
+++ work/curl/test.sh
@@ -10,9 +10,15 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-ulimit -n 19999
-echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle
-echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
+#ulimit -n 19999
+#echo 1 > /proc/sys/net/ipv4/tcp_tw_recycle
+#echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
 
-echo ./multi_test $@
-./multi_test $@
+desc=$1
+output=output.csv
+#./multi_test $@
+./multi_test -w 10000 -t 3000 -f data/ds_512.txt -d "${desc}" -o ${output}
+./multi_test -w 10000 -t 3000 -f data/ds_1k.txt -d "${desc}" -o ${output}
+./multi_test -w 10000 -t 3000 -f data/ds_10k.txt -d "${desc}" -o ${output}
+./multi_test -w 10000 -t 3000 -f data/ds_100k.txt -d "${desc}" -o ${output}
+./multi_test -w 10000 -t 3000 -f data/ds_1m.txt -d "${desc}" -o ${output}
