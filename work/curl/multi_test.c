cd853dc61258edbd7b80f6dea2d2fb594a89cefb
david-stratusee <david@stratusee.com>
2015-01-25 21:25:53 +0800

    remove work_list

--- work/curl/multi_test.c
+++ work/curl/multi_test.c
@@ -267,7 +267,7 @@ static int32_t check_available(CURLM *multi_handle, global_info_t *global_info, 
             easy_handle = msg->easy_handle;
             DUMP("easy_handle %p is done, code: %u-%s, msg_left: %d\n",
                     easy_handle, msg->data.result, curl_easy_strerror(msg->data.result),
-                    msg_left);
+                    msgs_left);
             DUMP("  %u:S[%u]-R[%u]-D[%u-%u]\n",
                     thread_info->idx, thread_info->work_done, thread_info->still_running,
                     thread_info->work_num, thread_info->succ_num);
