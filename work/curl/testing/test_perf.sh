5143a887ad086e43e542d6c2a5df8d3758a60d30
david-stratusee <david@stratusee.com>
2015-02-12 11:13:30 +0000

    fix

--- work/curl/testing/test_perf.sh
+++ work/curl/testing/test_perf.sh
@@ -8,37 +8,43 @@
 #      REVISION:  ---
 #===============================================================================
 
-set -o nounset                              # Treat unset variables as an error
+set${userstr} -o nounset                              # Treat unset variables as an error
 
 args="-a 40 -n 8 -b 8 -t 600"
 #list="ds_512.txt ds_100k.txt ds_1k.txt ds_10k.txt"
 list="512 100k 1k 10k"
+if [ $# -gt 0 ]; then
+    userstr=_$1
+else
+    userstr=""
+fi
 
+./add_route_2.sh del
 for file in $list; do
-    ./add_route_2.sh del
-
-    ../multi_test -f ../data/ds_${file}.txt -d direct -o ../result/aie_10min.csv ${args} >>../result/aie_10min.log 2>&1
+    ../multi_test -f ../data/ds_${file}.txt -d direct${userstr} -o ../result/aie_10min${userstr}.csv ${args} >>../result/aie_10min${userstr}.log 2>&1
     sleep 60
 
     ./add_route_3.sh
-    ../multi_test -f ../data/ds_${file}.txt -d 2c_aie -o ../result/aie_10min.csv ${args} >>../result/aie_10min.log 2>&1
+    ../multi_test -f ../data/ds_${file}.txt -d 2c{userstr} -o ../result/aie_10min${userstr}.csv ${args} >>../result/aie_10min${userstr}.log 2>&1
     ./add_route_3.sh del
     sleep 60
 
     ./add_route_2.sh
-    ../multi_test -f ../data/ds_${file}.txt -d 1c_aie -o ../result/aie_10min.csv ${args} >>../result/aie_10min.log 2>&1
+    ../multi_test -f ../data/ds_${file}.txt -d 1c{userstr} -o ../result/aie_10min${userstr}.csv ${args} >>../result/aie_10min${userstr}.log 2>&1
     ./add_route_2.sh del
     sleep 60
 
-    ../multi_test -f ../data/ds_${file}.txt -d s_direct -o ../result/aie_10min.csv -s ${args} >>../result/aie_10min.log 2>&1
+    ../multi_test -f ../data/ds_${file}.txt -d s_direct${userstr} -o ../result/aie_10min${userstr}.csv -s ${args} >>../result/aie_10min${userstr}.log 2>&1
     sleep 60
 
     ./add_route_3.sh
-    ../multi_test -f ../data/ds_${file}.txt -d s_2c_aie -o ../result/aie_10min.csv -s ${args} >>../result/aie_10min.log 2>&1
+    ../multi_test -f ../data/ds_${file}.txt -d s_2c{userstr} -o ../result/aie_10min${userstr}.csv -s ${args} >>../result/aie_10min${userstr}.log 2>&1
     ./add_route_3.sh del
     sleep 60
 
     ./add_route_2.sh
-    ../multi_test -f ../data/ds_${file}.txt -d s_1c_aie -o ../result/aie_10min.csv -s ${args} >>../result/aie_10min.log 2>&1
+    ../multi_test -f ../data/ds_${file}.txt -d s_1c{userstr} -o ../result/aie_10min${userstr}.csv -s ${args} >>../result/aie_10min${userstr}.log 2>&1
     ./add_route_2.sh del
+    sleep 60
 done
+
