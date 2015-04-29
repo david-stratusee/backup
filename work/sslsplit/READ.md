ccdd4f969c513eaafabd6b1ede021d68f16a3079
david-stratusee <david@stratusee.com>
2015-04-29 12:01:24 +0800

    add sslsplit

---
+++ work/sslsplit/READ.md
@@ -0,0 +1,4 @@
+* log function *
+logbuf_write_free: log thread call this to write data and free, and finally call log_content_common_writecb
+log_content_submit: submit content to log thread
+
---
+++ work/sslsplit/start_sslsplit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -
+#===============================================================================
+#          FILE: start_sslsplit1.sh
+#         USAGE: ./start_sslsplit1.sh
+#   DESCRIPTION:
+#        AUTHOR: dengwei
+#       CREATED: 2015年04月23日 01:19
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+sudo sysctl -w net.ipv4.ip_forward=1
+
+sudo iptables -t nat -F
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
+sudo iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 587 -j REDIRECT --to-ports 8443
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 465 -j REDIRECT --to-ports 8443
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 993 -j REDIRECT --to-ports 8443
+#sudo iptables -t nat -A PREROUTING -p tcp --dport 5222 -j REDIRECT --to-ports 8080
+sudo iptables -t nat -A POSTROUTING -j MASQUERADE
+
+sudo iptables -t nat -L
+
+rm -rf ./log_data
+mkdir ./log_data
+rm -f connections.log
+
+#sslsplit -D -l connections.log -S ./log_data/ -k /home/david/work/keys/squid_cert/holonet.key -c /home/david/work/keys/squid_cert/holonet.pem -C /home/david/work/keys/squid_cert/holonet_ca.pem ssl 0.0.0.0 8443 tcp 0.0.0.0 8080
+#sslsplit -l connections.log -S ./log_data/ -k /home/david/work/keys/squid_cert/holonet.key -c /home/david/work/keys/squid_cert/holonet.pem -C /home/david/work/keys/squid_cert/holonet_ca.pem ssl 0.0.0.0 8443 tcp 0.0.0.0 8080
+./sslsplit -P -l connections.log -S ./log_data/ -k /home/david/work/keys/squid_cert/holonet.key -c /home/david/work/keys/squid_cert/holonet.pem -C /home/david/work/keys/squid_cert/holonet_ca.pem ssl 0.0.0.0 8443 tcp 0.0.0.0 8080
