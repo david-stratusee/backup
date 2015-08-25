b418407e054b5a68ea9d6e6b29f1bf5977e7463c
david-stratusee <david@stratusee.com>
2015-08-25 13:08:53 +0800

    fix

--- bin/use_proxy.sh
+++ bin/use_proxy.sh
@@ -10,22 +10,41 @@
 
 set -o nounset                              # Treat unset variables as an error
 
+netname="en0"
+os_name=`uname -s`
+is_linux=0
+
 function proxy_help()
 {
     echo "$1 -c: delete proxy and restore route"
     echo "$1 -l: show proxy route"
-    echo "$1 -v: use vmware local proxy"
+    if [ ${is_linux} -eq 0 ]; then
+        echo "$1 -v: use vmware local proxy"
+    fi
     echo "$1 -b: use box aie proxy"
 }
 
 function reset_gw()
 {
-    echo route -n delete -net 0.0.0.0/0
-    sudo route -n delete -net 0.0.0.0/0
+    if [ ${is_linux} -gt 0 ]; then
+        echo route delete -net 0.0.0.0/0
+        sudo route delete -net 0.0.0.0/0
 
-    echo route -n add -net 0.0.0.0/0 $1
-    sudo route -n add -net 0.0.0.0/0 $1
+        echo route add -net 0.0.0.0/0 gw $1
+        sudo route add -net 0.0.0.0/0 gw $1
+    else
+        echo route -n delete -net 0.0.0.0/0
+        sudo route -n delete -net 0.0.0.0/0
+
+        echo route -n add -net 0.0.0.0/0 $1
+        sudo route -n add -net 0.0.0.0/0 $1
+    fi
 }
+
+if [ "${os_name}" == "Linux" ] || [ "${os_name}" == "linux" ]; then
+    is_linux=1
+    netname="eno16777736"
+fi
 
 if [ $# -eq 0 ] || [ "$1" == "-h" ]; then
     proxy_help `basename $0`
@@ -33,14 +52,31 @@ if [ $# -eq 0 ] || [ "$1" == "-h" ]; then
 fi
 
 if [ "$1" == "-c" ]; then
-    local_ip=`ifconfig en0 | egrep -o "inet .* netmask" | awk '{print $2}'`
+    local_ip=`ifconfig ${netname} | egrep -o "inet[ \t]+.*[ \t]+netmask" | awk '{print $2}'`
     gw=`echo $local_ip | awk -F"." '{print $1"."$2"."$3".1"}'`
     echo ${gw}
-
     reset_gw ${gw}
-elif [ "$1" == "-v" ]; then
+
+    if [ -f /etc/sysconfig/network-scripts/ifcfg-${netname} ]; then
+        sudo sed -i -e 's/^DEFROUTE=.*/DEFROUTE="yes"/g' /etc/sysconfig/network-scripts/ifcfg-${netname}
+        dhpid=`ps -ef | grep dhcl | grep ${netname} | awk '{print $2}'`
+        if [ "$dhpid" != "" ]; then
+            sudo kill $dhpid
+        fi
+        sudo dhclient ${netname}
+    fi
+elif [ ${is_linux} -eq 0 ] && [ "$1" == "-v" ]; then
     reset_gw 192.168.66.128
 elif [ "$1" == "-b" ]; then
+    if [ -f /etc/sysconfig/network-scripts/ifcfg-${netname} ]; then
+        sudo sed -i -e 's/^DEFROUTE=.*/DEFROUTE="no"/g' /etc/sysconfig/network-scripts/ifcfg-${netname}
+        dhpid=`ps -ef | grep dhcl | grep ${netname} | awk '{print $2}'`
+        if [ "$dhpid" != "" ]; then
+            sudo kill $dhpid
+        fi
+        sudo dhclient ${netname}
+    fi
+
     reset_gw 192.168.3.119
 elif [ "$1" != "-l" ]; then
     echo "unknown argument"
