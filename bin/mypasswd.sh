6bc302e3c4c957cd6ba40d6965b863b6b07568cd
david-stratusee <david@stratusee.com>
2015-09-16 00:34:58 +0800

    "fix"

--- bin/mypasswd.sh
+++ bin/mypasswd.sh
@@ -10,19 +10,27 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-if [ $# -lt 1 ]; then
-    echo "`basename $0` domain"
+function upper_echo()
+{
+	src="$1"
+	echo -e "$src" | awk '{print toupper($0)}'
+}
+
+if [ $# -lt 2 ]; then
+    echo "`basename $0` domain username"
     exit 1
 fi
 
+system=`uname -s`
+
 domain=$1
-password="dengwei-chen"
+password=$2
 domlen=${#domain}
 
 all_str=${domain}.${password}.${domlen}
 
 #echo $all_str
-md5_ret=`md5 -q -s $all_str`
+md5_ret=`mymd5 $all_str`
 #echo $md5_ret
 
 domlen=6
@@ -31,4 +39,4 @@ let ret2_len=16-ret2_start
 ret1=${md5_ret:0:${domlen}} 
 ret2=${md5_ret:${ret2_start}:${ret2_len}} 
 
-echo $ret1@$ret2
+echo $ret1@`upper_echo $ret2`
