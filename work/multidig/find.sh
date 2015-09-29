946a35b4a5fb735a5f2afed1232f22c5bc0e6678
david-stratusee <david@stratusee.com>
2015-09-29 15:17:07 +0800

    fix

---
+++ work/multidig/find.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -
+#===============================================================================
+#          FILE: find.sh
+#         USAGE: ./find.sh
+#        AUTHOR: dengwei, david@stratusee.com
+#       CREATED: 2015/09/29 14:08
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+while read line; do
+    echo $line
+    ./multi_dns_query.py $line
+    ./get_fastest_ip.sh ${line}"_result.log" >ip.log
+    sort -k2 -g ip.log -o ${line}"_ip_sort.log"
+    rm -f ${line}"_result.log" ip.log
+done <$1
+
